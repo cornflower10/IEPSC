8f13173be8566e37b4e0109f88f5417a350fcdcf
cornflower10 <861281331@qq.com>
2016-04-12 20:31:45 +0800

    commit

--- app/src/main/java/cn/cornflower/com/huan/ui/activity/fragment/FinshTaskFragment.java
+++ app/src/main/java/cn/cornflower/com/huan/ui/activity/fragment/FinshTaskFragment.java
@@ -14,12 +14,14 @@ import java.util.List;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import cn.cornflower.com.huan.R;
+import cn.cornflower.com.huan.adapter.TaskAdapter;
+import cn.cornflower.com.huan.common.Constants;
 import cn.cornflower.com.huan.entity.Task;
 
 /**
  * Created by Administrator on 2016/4/5.
  */
-public class FinshTaskFragment_ extends Fragment {
+public class FinshTaskFragment extends Fragment {
     @InjectView(R.id.lv_task)
     ListView lvTask;
     private List<Task> list;
@@ -32,19 +34,28 @@ public class FinshTaskFragment_ extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_finish_task, container, false);
         ButterKnife.inject(this, view);
+        initData();
+
         lvTask.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
             }
         });
-        initData();
+
         return view;
     }
 
     private void initData() {
-        
-//        lvTask.setAdapter(new TaskAdapter(getActivity(),list));
+
+      Bundle bundle  = getArguments();
+        if(null!= bundle){
+            list = bundle.getParcelableArrayList(Constants.TASKLIST);
+        }
+
+
+
+        lvTask.setAdapter(new TaskAdapter(getActivity(),list));
     }
 
     @Override
